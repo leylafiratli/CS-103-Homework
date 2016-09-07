6f2a196e7577a7d9450a9820fcacdb26c1de2561
Samuel-Resendez <saresend@usc.edu>
2016-09-06 20:11:47 -0700

    Implemented hailstone part 1

--- hailstone.cpp
+++ hailstone.cpp
@@ -1,1 +1,47 @@
 // Author: Samuel Resendez
+//Date: September 6th, 2016
+
+
+////Boilerplate Box ///
+#include <iostream> ///
+#include <cmath>    ///
+                    ///
+using namespace std;///
+///////////////////////
+
+
+// --------------------- Begin Implementation ----------------- //
+
+
+
+int main() {
+	//-------------------initializing variables -------------- //
+	int userInput;
+	int loopCounter = 0; // used for counting number of loops needed to reach 1
+// -----------------------------------------------------------//
+
+	//Take user input
+	cout << "Please, enter a number: ";
+	cin >> userInput;
+
+	while(userInput != 1) {
+		loopCounter++;
+		if(userInput % 2 == 0) { // If the number is EVEN
+			userInput /= 2;
+
+		}
+		else { // If the number is ODD
+			userInput = userInput * 3 + 1;
+		}
+		cout << userInput << " ";
+	}
+	cout << endl;
+	cout << "Length: " << loopCounter << endl;
+
+
+
+
+
+
+	return 0;
+}
