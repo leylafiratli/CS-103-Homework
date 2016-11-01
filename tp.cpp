5e33f516c18331a2e20d6d488ea66bc326d7cb7c
Samuel Resendez <saresend@usc.edu>
2016-10-31 21:30:41 -0700

    midTermPractice folder

---
+++ tp.cpp
@@ -0,0 +1,35 @@
+/* Author: Samuel Resendez
+   Date: Wednesday, September 14th, 2016
+   
+*/
+
+// -------- Boilerplate -------
+#include <iostream>
+#include <cmath>
+#include <stdlib.h>
+
+using namespace std;
+// ------ end boilerplate -------
+
+
+// ------- method prototypes
+double rand_uniform();
+int single_sim(int,double);
+
+
+int main() {
+// Variable declarations 
+
+  int N;
+  double P;
+  /* set the seed */
+  srand(time(0));
+  /*------------*/
+  
+
+
+} 
+int single_sim(int N, double p) {
+  if(p == .50) {
+  }
+}
